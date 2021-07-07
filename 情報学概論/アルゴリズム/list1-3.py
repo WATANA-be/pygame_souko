e7138d355d19f5372a2106a869becf04056fc4cd
WATANA-be <aansssr@gmail.com>
2021-07-07 23:57:17 +0900

    aaaaa

---
+++ 情報学概論/アルゴリズム/list1-3.py
@@ -0,0 +1,8 @@
+n = int(input('整数:'))
+
+if n>0:
+    print('その値は正です')
+elif n<0:
+    print('その値は負です')
+else:
+    print('その値は0です')
---
+++ 情報学概論/アルゴリズム/list1-7.py
@@ -0,0 +1,11 @@
+print('１からnまでの総和を求めます')
+n = int(input('nの値'))
+
+sum = 0
+i = 1
+
+while i <= n:
+    sum += i
+    i += 1
+
+print(f"1から{n}までの総和は{sum}です")
